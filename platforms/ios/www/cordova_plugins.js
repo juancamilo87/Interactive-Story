9ff70424836a044372ecdac7f5fed5f3a0d86efe
Juan Camilo Garcia <juancamilo87@outlook.com>
2015-03-11 21:01:30 -0500

    Added Console plugin for iOS and WP8

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,6 +1,20 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/com.brodysoft.sqlitePlugin/www/SQLitePlugin.js",
+        "id": "com.brodysoft.sqlitePlugin.SQLitePlugin",
+        "clobbers": [
+            "SQLitePlugin"
+        ]
+    },
+    {
+        "file": "plugins/com.intelegate.cordova.plugin.LocalStoragePlugin/www/LocalStoragePlugin.js",
+        "id": "com.intelegate.cordova.plugin.LocalStoragePlugin.LocalStoragePlugin",
+        "clobbers": [
+            "LocalStoragePlugin"
+        ]
+    },
+    {
         "file": "plugins/org.apache.cordova.console/www/console-via-logger.js",
         "id": "org.apache.cordova.console.console",
         "clobbers": [
@@ -25,6 +39,8 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "com.brodysoft.sqlitePlugin": "1.0.4",
+    "com.intelegate.cordova.plugin.LocalStoragePlugin": "0.0.4",
     "org.apache.cordova.console": "0.2.13",
     "org.apache.cordova.device": "0.3.0"
 }
---
+++ platforms/ios/www/plugins/com.intelegate.cordova.plugin.LocalStoragePlugin/www/LocalStoragePlugin.js
@@ -0,0 +1,35 @@
+cordova.define("com.intelegate.cordova.plugin.LocalStoragePlugin.LocalStoragePlugin", function(require, exports, module) { var argscheck = require('cordova/argscheck'),
+    utils = require('cordova/utils'),
+    exec = require('cordova/exec');
+
+var localStoragePlugin = {
+    addKeyValuePair: function(type, key, value, successCallback, errorCallback) {
+        exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'LocalStoragePlugin', // mapped to our native Java class called "CalendarPlugin"
+            'addKeyValuePair', // with this action name
+            [{                  // and this array of custom arguments to create our entry
+                "type": type,
+                "key": key,
+                "value": value
+            }]
+        ); 
+     },
+     getKeyValuePair: function(type, key, deFaultValue, successCallback, errorCallback) {
+        exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'LocalStoragePlugin', // mapped to our native Java class called "CalendarPlugin"
+            'getKeyValuePair', // with this action name
+            [{                  // and this array of custom arguments to create our entry
+                "type": type,
+                "key": key,
+                "deFaultValue": deFaultValue
+            }]
+        ); 
+     }
+};
+
+module.exports = localStoragePlugin;
+});
