40cab339d5faeeff623ae3379c367aca2fabadcf
Jilin Yang <jilin.yang@student.oulu.fi>
2015-03-26 19:17:39 +0200

    Yifei modified story_db.js file to include logs of stories and interactions

--- www/js/story_db.js
+++ www/js/story_db.js
@@ -1,4 +1,10 @@
 version = '1.1';
+
+////yifei added here
+var story_log = null;
+var interaction_log = null;
+////
+
 document.addEventListener("deviceready", onDeviceReady, false);
 var db;
 function onDeviceReady() {
@@ -49,6 +55,11 @@ function populateDB(tx) {
     tx.executeSql('CREATE TABLE quiz (quiz_id INTEGER PRIMARY KEY AUTOINCREMENT, question TEXT NOT NULL, correct_answer TEXT NOT NULL, answer_1 TEXT NOT NULL, answer_2 TEXT NOT NULL, answer_3 TEXT NOT NULL, feedback_right TEXT, feedback_wrong TEXT, audiopath_right TEXT, audiopath_wrong TEXT)');             
     tx.executeSql('CREATE TABLE versions (version TEXT)');
     tx.executeSql('INSERT INTO versions (version) VALUES (?)', [version]);
+    
+    ////Create tables related to logs(Yifei)
+    tx.executeSql('CREATE TABLE story_log (action TEXT, status INTEGER, chapter_id INTEGER, story_id INTEGER, timestamp TEXT, PRIMARY KEY(chapter_id, story_id, timestamp))');
+    tx.executeSql('CREATE TABLE interaction_log (interaction_type TEXT, interaction_id INTEGER, interaction_status INTEGER, timestamp TEXT, PRIMARY KEY(interaction_type, interation_id, timestamp))');
+    ////
 }
 
 // create tables
@@ -83,4 +94,61 @@ function successCB() {
 function successdropCB() {
     console.log('Regenerating DB');
     db.transaction(populateDB, errorCB, successCB);
+}
+
+
+
+//// Function for submitting logs on-line by users; Send to server Not finished yet(Yifei)
+function submit_logs(tx){
+    ////Get story logs and interaction logs, then combine them together. 
+    tx.executeSql('select * from story_log', [], success_get_story_log);
+    tx.executeSql('select * from interaction_log', [], success_get_interaction_log);
+    ////use story_log to merge interaction_log, just send story_log
+    $.extend(story_log, interaction_log);
+    
+    ////Send logs to server by HTTP, needed to be implement
+    
+}
+
+
+//// Call back function, return query results of story logs (Yifei)
+function success_get_story_log(tx, res){
+    var jsonObject = {};
+    for (var i = 0; i < res.rows.length; i ++){
+        var a = res.rows.item(i)["action"];
+        var s = res.rows.item(i)["status"];
+        var c_i = res.rows.item(i)["chapter_id"];
+        var s_i = res.rows.item(i)["story_id"];
+        var t = res.rows.item(i)["timestamp"];
+        var each_item = {"action": a, "status": s, "chapter_id": c_i, "story_id": s_i; "timestamp": t};
+        jsonObject["story_log"].push(each_item);
+    }
+    story_log = jsonObject;    
+}
+
+//// Call back function, return query results of interaction logs (Yifei)
+function success_get_interaction_log(tx, res){
+    var jsonObject = {};
+    for (var i = 0; i < res.rows.length; i ++){
+        var i_t = res.rows.item(i)["interaction_type"];
+        var i_i = res.rows.item(i)["interaction_id"];
+        var i_s = res.rows.item(i)["interaction_status"];
+        var t = res.rows.item(i)["timestamp"];
+        var each_item = {"interaction_type": a, "interaction_id": s, "interaction_status": c, "timestamp": t};
+        jsonObject["interaction_log"].push(each_item);
+    }
+    interaction_log = jsonObject;
+}
+
+
+////logs for story (Yifei)
+function story_log(tx, action, status, chapter_id, story_id){
+    var timestamp = Date.now();
+    tx.execute('INSERT INTO story_log (action, status, chapter_id, story_id, timestamp) VALUES (?,?,?,?)', [action, status, chapter_id, story_id, timestamp]);
+}
+
+////logs for interaction (Yifei)
+function interaction_log(tx, interaction_type, interaction_id, interaction_status){
+    var timestamp = Date.now();
+    tx.execute('INSERT INTO interaction_log (interaction_type, interaction_id, interaction_status, timestamp) VALUES (?,?,?,?)', [interaction_type, interaction_id, interaction_status, timestamp]);
 }
