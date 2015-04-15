822385d31e1c27a279d2cc234751bd26c6d9ac16
Zhanna Sarsenbayeva <zhanna.sarsenbayeva@gmail.com>
2015-04-15 20:09:16 +0300

    quiz changed
    quiz.html and quiz.js are added, quiz is working now fine

--- www/js/quiz.js
+++ www/js/quiz.js
@@ -1,4 +1,18 @@
-function check_quiz(correct_answer, selected_answer){
-	selected_answer = 
-	document.write("Answers submitted");
+function check_quiz(correct_answer){
+	var form = document.getElementById("question_form");
+	var radios = form.elements["question1"];
+	var val=0;
+	var is_correct = false;
+	
+	for (var i=0, len=radios.length; i<len; i++) {
+		        if ( radios[i].checked ) { // radio checked?
+		            val = radios[i].value; // if so, hold its value in val
+		            break; // and break out of for loop
+		        }
+		    }
+	if (val==correct_answer){
+		is_correct = true;
+	}
+	    
+	document.write(is_correct);
 }
