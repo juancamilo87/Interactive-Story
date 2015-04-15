99a5336148a92fcaac2d91e9d5c19adc20f2bbe4
Furqan Ahmed <ahmedfurqan88@gmail.com>
2015-04-15 20:44:37 +0300

    committing the index.js for populating the database

--- www/js/index.js
+++ www/js/index.js
@@ -33,6 +33,8 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+    	
+    	app.populateDb();
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
